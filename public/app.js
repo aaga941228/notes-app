870e4ec11c8c25b65c7b96ef05afbd68329e2355
aaga <aaga941228@outlook.es>
2019-11-25 12:47:31 -0600

    Houston, we are offline!!!

--- public/app.js
+++ public/app.js
@@ -1,20 +1,26 @@
 window.addEventListener("load", () => {
+  window.addEventListener("online", () => {
+    doPendings();
+  });
+  let counter = 0;
+  const notesService = NotesService();
+  // const requestsQueue = RequestsQueue();
   const notes = document.getElementById("notes");
   const message = document.getElementById("message-container");
   const plus = document.getElementById("plus");
   plus.addEventListener("click", addNote);
   const note = item => `
     <div class="title-x">
-      <input type="text" value="${item.title}" id='txt-title-${item.id}' class="txt-title"/>
-      <button class="close" id='btn-${item.id}'>&times;</button>
+    <input type="text" value="${item.title}" id='txt-title-${item.id}' class="txt-title"/>
+    <button class="close" id='btn-${item.id}'>&times;</button>
     </div>
     <textarea type="text" id='txt-note-${item.id}' class="txt-note">${item.note}</textarea>
     `;
 
-  // fetch notes
+  /****************** fetchNotes ***************************/
 
-  async function fetchNotes() {
-    const data = await getNotes();
+  async function init() {
+    const data = await notesService.getNotes();
     if (data.length === 0) {
       addMessage();
     } else {
@@ -24,7 +30,16 @@ window.addEventListener("load", () => {
     }
   }
 
-  // functions
+  /****************** functions ***************************/
+
+  const ID = function() {
+    return (
+      "_" +
+      Math.random()
+        .toString(36)
+        .substr(2, 9)
+    );
+  };
 
   function addMessage() {
     message.style.display = "flex";
@@ -36,17 +51,17 @@ window.addEventListener("load", () => {
 
   function createNote(item) {
     const node = document.createElement("div");
+    node.setAttribute("id", `note-${item.id}`);
     node.innerHTML = note(item);
     node.classList.add("note");
-    node.setAttribute("id", `note-${item.id}`);
     notes.appendChild(node);
-    closeNote(item.id);
-    save(item.id);
+    saveNote(item.id);
     sizeTxtArea(item.id);
+    closeNote(item.id);
   }
 
   async function addNote() {
-    const newNote = await postNote();
+    const newNote = await notesService.postNote();
     createNote(newNote);
     quitMessage();
   }
@@ -56,8 +71,8 @@ window.addEventListener("load", () => {
     const note = document.getElementById(`note-${id}`);
     button.addEventListener("click", e => {
       e.preventDefault();
-      deleteNote(id);
       const { parentNode } = note;
+      notesService.deleteNote(id);
       parentNode.removeChild(note);
       if (parentNode.childElementCount === 1) {
         addMessage();
@@ -68,17 +83,17 @@ window.addEventListener("load", () => {
   function confirmSave(id) {
     const txtArea = document.getElementById(`txt-note-${id}`);
     const title = document.getElementById(`txt-title-${id}`).value;
-    const note = document.getElementById(`txt-note-${id}`).value;
+    const note = txtArea.value;
     const data = {
       title,
       note
     };
     const txtAreaHeight = txtArea.offsetHeight;
     localStorage.setItem(`size-${id}`, `${txtAreaHeight}px`);
-    putNote(data, id);
+    notesService.putNote(data, id);
   }
 
-  function save(id) {
+  function saveNote(id) {
     const txtArea = document.getElementById(`txt-note-${id}`);
     txtArea.addEventListener("keyup", event => {
       if (event.keyCode === 13) {
@@ -90,7 +105,7 @@ window.addEventListener("load", () => {
       interval = setInterval(() => {
         event.preventDefault();
         confirmSave(id);
-      }, 5000);
+      }, 10000);
     });
     txtArea.addEventListener("blur", () => {
       clearInterval(interval);
@@ -105,45 +120,129 @@ window.addEventListener("load", () => {
     }
   }
 
-  // request functions
+  /****************** PendingRequets ***************************/
 
-  async function getNotes() {
-    const res = await fetch("http://localhost:8080");
-    const data = await res.json();
-    return data;
+  // async function doPendingRequets() {
+  //   const req = requestsQueue.dequeue();
+  //   if (requestsQueue.isEmpty()) {
+  //     doPendingRequets();
+  //   }
+  //   return;
+  // }
+
+  async function doPendingRequests(req) {
+    const request = JSON.parse(req);
+    switch (request.method) {
+      case "DELETE":
+        await notesService.deleteNote(request.id);
+        break;
+      case "PUT":
+        await notesService.putNote(request.data, request.id);
+        break;
+      case "POST":
+        await notesService.postNote(request.id);
+        break;
+      default:
+        break;
+    }
+  }
+  function removePendingRequests(id) {
+    localStorage.removeItem(`request-${id}`);
   }
 
-  async function putNote(data, id) {
-    await fetch(`http://localhost:8080/${id}`, {
-      method: "PUT",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
+  async function doPendings() {
+    for (let i = 0; i < counter; i++) {
+      const req = localStorage.getItem(`request-${i}`);
+      await doPendingRequests(req);
+      removePendingRequests(i);
+    }
+    counter = 0;
+  }
+
+  /****************** NotesService ***************************/
+
+  function NotesService() {
+    async function getNotes() {
+      const res = await fetch("http://localhost:8080");
+      const data = await res.json();
+      return data;
+    }
+
+    async function putNote(data, id) {
+      if (navigator.onLine) {
+        await fetch(`http://localhost:8080/${id}`, {
+          method: "PUT",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+      } else {
+        enqueueRequest({ id, data, method: "PUT" });
       }
-    });
-  }
+    }
 
-  async function postNote() {
-    const response = await fetch(`http://localhost:8080/`, {
-      method: "POST",
-      body: JSON.stringify({ title: "", note: "" }),
-      headers: {
-        "Content-Type": "application/json"
+    async function postNote(id) {
+      if (navigator.onLine) {
+        if (id === undefined) {
+          const response = await fetch(`http://localhost:8080/`, {
+            method: "POST",
+            body: JSON.stringify({ title: "", note: "" }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+          const res = await response.json();
+          const newNote = {
+            title: "",
+            note: "",
+            id: res.id
+          };
+          return newNote;
+        } else {
+          const response = await fetch(`http://localhost:8080/`, {
+            method: "POST",
+            body: JSON.stringify({ id, title: "", note: "" }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+          const res = await response.json();
+          return res;
+        }
+      } else {
+        const id = ID();
+        enqueueRequest({ id, method: "POST" });
+        return { id, title: "", note: "" };
       }
-    });
-    const res = await response.json();
-    const newNote = { title: "", note: "", id: res.id };
-    return newNote;
+    }
+
+    async function deleteNote(id) {
+      if (navigator.onLine) {
+        await fetch(`http://localhost:8080/${id}`, {
+          method: "DELETE",
+          headers: { "content-type": "application/json" }
+        });
+      } else {
+        enqueueRequest({ id, method: "DELETE" });
+      }
+    }
+
+    return {
+      getNotes,
+      postNote,
+      putNote,
+      deleteNote
+    };
   }
 
-  async function deleteNote(id) {
-    await fetch(`http://localhost:8080/${id}`, {
-      method: "DELETE",
-      headers: { "content-type": "application/json" }
-    });
+  /****************** RequestsQueue ***************************/
+
+  function enqueueRequest(element) {
+    localStorage.setItem(`request-${counter}`, JSON.stringify(element));
+    counter++;
   }
 
-  // init
-
-  fetchNotes();
+  /********************** init *******************************/
+  init();
 });
